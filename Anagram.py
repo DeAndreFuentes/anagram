5df10c1ce5fb8578a685ecbd8032ace7389cc834
DeAndreFuentes <74945613+DeAndreFuentes@users.noreply.github.com>
2021-04-04 16:01:18 -0400

    Add files via upload

---
+++ Anagram.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+def insp_anagram(word1, word2):
+    return sorted(word1) == sorted(word2)
+
+insp_anagram("bat", "tab")
+
+
+
+# In[ ]:
+
+
+
+
